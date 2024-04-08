4de427dde3b51b36b97e4d85c8ddba7a794c5d5f
German Steven Méndez Clavel <clavelsteven441@gmail.com>
2024-04-08 09:55:33 -0600

    capa logica de negocio hecha

---
+++ G20240408.LogicaDeNegocio/DependecyContainer.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using G20240408.AccesoADatos;
+namespace G20240408.LogicaDeNegocio
+{
+    public static class DependecyContainer
+    {
+        public static IServiceCollection AddBLDependecies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDALDependecies(configuration);
+            services.AddScoped<PersonaGBL>();
+            return services;
+        }
+
+    }
+}
---
+++ G20240408.LogicaDeNegocio/PersonaGBL.cs
@@ -0,0 +1,39 @@
+﻿using G20240408.AccesoADatos;
+using G20240408.EntidadesDeNegocio;
+
+namespace G20240408.LogicaDeNegocio
+{
+    public class PersonaGBL
+    {
+        readonly PersonaGDAL _personaGDAL;
+        public PersonaGBL(PersonaGDAL personaGDAL)
+        {
+            _personaGDAL = personaGDAL;
+        }
+
+        public async Task<int> Crear(PersonaG personaG)
+        {
+            return await _personaGDAL.Crear(personaG);
+        }
+        public async Task<int> Modificar(PersonaG personaG)
+        {
+            return await _personaGDAL.Modificar(personaG);
+        }
+        public async Task<int> Eliminar(PersonaG personaG)
+        {
+            return await _personaGDAL.Eliminar(personaG);
+        }
+        public async Task<PersonaG> ObtenerPorId(PersonaG personaG)
+        {
+            return await _personaGDAL.ObtenerPorId(personaG);
+        }
+        public async Task<List<PersonaG>> ObtenerTodos()
+        {
+            return await _personaGDAL.ObtenerTodos();
+        }
+        public async Task<List<PersonaG>> Buscar(PersonaG personaG)
+        {
+            return await _personaGDAL.Buscar(personaG);
+        }
+    }
+}
